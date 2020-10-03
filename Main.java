576f5e33702a2d0a732f7ecddc0b6b7f45defebf
Brayam <brayamcorral@gmail.com>
2020-10-03 00:07:05 -0700

    Base

--- Main.java
+++ Main.java
@@ -3,10 +3,8 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Player p1 = new Player();
-		p1.shoot();
-		p1.reload();
-		p1.block();
+		
+		/* Sam */
 	}
 	
 }
