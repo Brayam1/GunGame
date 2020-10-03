87f74c9f2f1b423024500b961222ce64f86d516c
Brayam1 <brayamcorral@gmail.com>
2020-10-02 20:42:28 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+	public void main() {
+		Player p1 = new Player();
+		p1.shoot();
+		p1.reload();
+		p1.block();
+	}
+	
+}
---
+++ Player.java
@@ -0,0 +1,20 @@
+
+public class Player {
+
+	
+	public void shoot() {
+		/* implement me */
+	}
+	
+	
+	public void reload() {
+		/* implement me */
+	}
+	
+	
+	public void block() {
+		/* implement me */
+	}
+	
+	
+}
