92c377c93d03d7a3406fdc7ea88f89710e56fdd4
Edgar Ubaldo <fakeemail@email.com>
2020-10-03 19:35:39 -0700

    Updated to add public/private stuff

--- CPU.java
+++ CPU.java
@@ -2,12 +2,18 @@
 import java.util.Random;
 
 public class CPU {
-	int score = 0;
-	GunStance gun = new GunStance();
-	Random rand = new Random();
+	private int score = 0;
+	public GunStance gun = new GunStance();
+	private Random rand = new Random();
 	
 	//reason for p1 bullets is so we can make it smarter
-	int getAction(int p1_bullets) {
+	
+	public int getScore() {return score;}
+	
+	public void incrementScore() {score++;}
+	
+	
+	public int getAction(int p1_bullets) {
 		if(gun.bullet > 0) {
 			return gun.setAction(rand.nextInt(3));
 		}
