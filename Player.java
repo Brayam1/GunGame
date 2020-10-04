791e97ddbee6177d52dcfb74508960fae581e71b
Brayam <brayamcorral@gmail.com>
2020-10-03 19:45:38 -0700

    a

--- Player.java
+++ Player.java
@@ -1,20 +1,38 @@
+import java.util.Scanner;
 
 public class Player {
 
+	private int input;
+	private int score = 0;
+	public GunStance gun = new GunStance();
 	
-	public void shoot() {
-		/* implement me */
+	public int getAction(){
+		
+		String[] actions = {"shield", "shoot", "reload"};
+		
+		Scanner scan = new Scanner(System.in);	
+		
+		System.out.print("Choose Action: " + "\n" + "0:" + actions[0] + " 1:" + actions[1] + " 2:" + actions[2] + "\n");
+		input = scan.nextInt();
+		
+		while(input < 0 || input > 2) {
+				System.out.print("Invalid action. \n");
+				System.out.print("Choose Action: " + "\n" + "0:" + actions[0] + " 1:" + actions[1] + " 2:" + actions[2] + "\n");
+				input = scan.nextInt();
+		}
+		
+		System.out.println("you chose: " + actions[input]);
+		
+		scan.close();
+		return gun.setAction(input);
+     
 	}
 	
-	
-	public void reload() {
-		/* implement me */
+	public int getScore() {
+		return score;
 	}
 	
-	
-	public void block() {
-		/* implement me */
+	public void incrementScore() {
+		score++;
 	}
-	
-	
 }
